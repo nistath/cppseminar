065ef92b9ac520654c11012f1a41efac9d6eed78
Nick Stathas <nickstathas@outlook.com>
2016-12-09 18:06:55 +0200

    Update fstream.cpp

--- Day 3/fstream.cpp
+++ Day 3/fstream.cpp
@@ -2,12 +2,18 @@
 using namespace std;
 
 int main() {
-    //ifstream in("input.txt");
+    ifstream in("input.txt");
     ofstream out("output.txt");
-
+    
+    int k;
+    in>>k;
+    k = k * 2;
+    
     out<<"POTATO!";
+    out<<k<<endl; // endl changes lines
+    out<<"PO"<<"TA"<<"TO"; // chaining << operators results in a "POTATO" output
 
-    //in.close();
+    in.close();
     out.close(); // Close the stream when you're done with it.
 
     return 0;
